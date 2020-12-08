62ede5a9f2269e372e34d2020a04f4e711035181
luk-str <lstrociak@gmail.com>
2020-12-08 13:48:39 +0100

    Add search parameters + fetch random collection

--- lib/fetchFromArchive.tsx
+++ lib/fetchFromArchive.tsx
@@ -1,5 +1,26 @@
 import axios from "axios";
 
+const searchPagesRange: number = 100;
+const resultsAmount: number = 10;
+
+const collections: string[] = [
+  "78rpm",
+  "78rpm_bostonpubliclibrary",
+  "78rpm_chomowicz_ready",
+  "78rpm_mcneil",
+  "78rpm_thorpe",
+  "album_recordings",
+  "pogonyi-cds",
+  "unlockedrecordings",
+];
+
+function getRandomCollection(): string {
+  const randomIndex: number = Math.floor(Math.random() * collections.length);
+  return collections[randomIndex];
+}
+
 async function getItemId() {}
 async function getFileList(itemId: string) {}
 function getSource(fileList: string[], itemId: string) {}
+
+export { getRandomCollection };
