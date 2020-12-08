879b637405c11e9d7a34bfa810413edce20ca64b
luk-str <lstrociak@gmail.com>
2020-12-08 11:52:09 +0100

    Initial clean-up

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -3,5 +3,5 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
-  res.json({ name: "John Doe" });
+  res.json({ message: "Got it!" });
 };
