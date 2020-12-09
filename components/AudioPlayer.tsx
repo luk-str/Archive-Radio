13400f22d520bd985744dcb5876c6bbe2a73a782
luk-str <lstrociak@gmail.com>
2020-12-09 11:03:12 +0100

    Add basic play/pause function

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,9 +1,33 @@
+import { useRef } from "react";
+
 export default function AudioPlayer() {
+  const audioElement = useRef<HTMLAudioElement>(null);
+
+  const playPause: () => void = () => {
+    const audio = audioElement.current;
+
+    if (audio.src === "") {
+      const testSource: string =
+        "https://ia801907.us.archive.org/2/items/lucien-muratore-monella-dufilm-le-chanteur-inconnu-pathe-x-93040/LucienMuratore,Monella,DufilmLeChanteurInconnu,PatheX93040.mp3";
+
+      audio.src = testSource;
+    }
+
+    if (audio.paused) {
+      audio.play();
+      console.log("playing");
+    } else {
+      audio.pause();
+      console.log("paused");
+    }
+  };
+
   return (
     <div>
       <h2>Title</h2>
+      <audio ref={audioElement}></audio>
       <h3>[Progress bar]</h3>
-      <button>Play/Pause</button>
+      <button onClick={playPause}>Play/Pause</button>
     </div>
   );
 }
