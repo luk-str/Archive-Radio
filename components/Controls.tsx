28cdde797bb0858c7614015939662d2959221565
luk-str <lstrociak@gmail.com>
2020-12-23 13:28:34 +0100

    Add SVG Icons for buttons

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,4 +1,10 @@
 import styles from "./Controls.module.css";
+import {
+  PreviousTrackButtonSVG,
+  NextTrackButtonSVG,
+  PlayButtonSVG,
+  PauseButtonSVG,
+} from "./svg/buttons";
 
 type Props = {
   loadPreviousTrack: () => void;
@@ -20,36 +26,56 @@ const Controls = ({
 }: Props) => {
   return (
     <nav>
-      <input
-        type="checkbox"
-        id="autoplay"
-        name="autoplay"
-        checked={isAutoplayOn}
-        onChange={toggleAutoplay}
-      />
-      <label htmlFor="autoplay" 
-      className={styles.checkboxLabel}
-      >
-        autoplay
-      </label>
+      <section>
+        <input
+          type="checkbox"
+          id="autoplay"
+          name="autoplay"
+          checked={isAutoplayOn}
+          onChange={toggleAutoplay}
+        />
+        <label htmlFor="autoplay" className={styles.checkboxLabel}>
+          autoplay
+        </label>
+      </section>
 
-      <button
-        onClick={() => loadPreviousTrack()}
-        className={styles.changeTrackButton}
-      >
-        ☜
-      </button>
+      <section className={styles.container}>
+        <button
+          onClick={() => loadPreviousTrack()}
+          className={styles.changeTrackButton}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className={styles.svgButton}
+          >
+            <PreviousTrackButtonSVG />
+          </svg>
+        </button>
 
-      <button onClick={playPause} className={styles.changeTrackButton}>
-        {!isPlaying ? "►" : "∥∥"}
-      </button>
+        <button onClick={playPause} className={styles.changeTrackButton}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className={styles.svgButton}
+          >
+            {!isPlaying ? <PlayButtonSVG /> : <PauseButtonSVG />}
+          </svg>
+        </button>
 
-      <button
-        onClick={() => loadNextTrack()}
-        className={styles.changeTrackButton}
-      >
-        ☞
-      </button>
+        <button
+          onClick={() => loadNextTrack()}
+          className={styles.changeTrackButton}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className={styles.svgButton}
+          >
+            <NextTrackButtonSVG />
+          </svg>
+        </button>
+      </section>
     </nav>
   );
 };
