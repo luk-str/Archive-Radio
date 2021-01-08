809eb2b76c87bd2eea46534827f10b9b57b52f49
luk-str <lstrociak@gmail.com>
2021-01-08 15:39:36 +0100

    Refactor code

--- components/AlbumArt.tsx
+++ components/AlbumArt.tsx
@@ -1,25 +1,21 @@
-import Image from "next/image";
 import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 import styles from "./AlbumArt.module.css";
-
-const placeholderImagePath: string = "/img/cover-fallback.png";
 
 type Props = {
   imageSourceUrl: string;
 };
 
-const AlbumArt = ({ imageSourceUrl }: Props) => {
+export default function AlbumArt({ imageSourceUrl }: Props) {
   const [imageSource, setImageSource] = useState<string>(imageSourceUrl);
   const [isImageReady, setIsImageReady] = useState<boolean>(false);
+
+  const placeholderImagePath = "/img/cover-fallback.png";
 
   function handleOnLoad(e: ChangeEvent<HTMLImageElement>) {
     if (e.target.srcset) {
       setIsImageReady(true);
     }
-  }
-
-  function handleOnError() {
-    setImageSource(placeholderImagePath);
   }
 
   return (
@@ -31,11 +27,9 @@ const AlbumArt = ({ imageSourceUrl }: Props) => {
         width="200"
         height="200"
         onLoad={handleOnLoad}
-        onError={handleOnError}
+        onError={() => setImageSource(placeholderImagePath)}
         className={isImageReady ? styles.visible : styles.hidden}
       />
     </>
   );
-};
-
-export default AlbumArt;
+}
