3de5cfddf43db6bea8b85e51719c3de64c3f90c6
luk-str <lstrociak@gmail.com>
2021-01-08 16:25:00 +0100

    Refactor code

--- lib/convertMetadata.ts
+++ lib/convertMetadata.ts
@@ -1,8 +1,8 @@
 export function convertSecondsToMinSec(value: number): string {
-  const minutes: number = Math.floor(value / 60);
-  const seconds: number = Math.floor(value % 60);
+  const minutes = Math.floor(value / 60);
+  const seconds = Math.floor(value % 60);
 
-  const secondsPadded: string = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const secondsPadded = seconds < 10 ? `0${seconds}` : `${seconds}`;
 
   return `${minutes}:${secondsPadded}`;
 }
