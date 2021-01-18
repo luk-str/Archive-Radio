d729d1fd7eb2836cd6d5781fd8415c6e115181fc
luk-str <lstrociak@gmail.com>
2021-01-18 15:42:13 +0100

    Link header title to homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,9 @@ import styles from "./Header.module.css";
 
 const Header = () => (
   <header className={styles.container}>
-    <h1 className={styles.title}>Archive Radio</h1>
+    <a href="/" title="Reload Archive Radio">
+      <h1 className={styles.title}>Archive Radio</h1>
+    </a>
   </header>
 );
 
