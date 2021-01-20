c8d2221b852bd8d27a19afe777c060af60ebdb93
luk-str <lstrociak@gmail.com>
2021-01-20 20:16:25 +0100

    Add copied alert after clicking copy button

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -1,5 +1,6 @@
 import styles from "./ShareModal.module.css";
 import { AiOutlineCopy } from "react-icons/ai";
+import { useRef } from "react";
 
 type Props = {
   shareTrackUrl: string;
@@ -7,19 +8,32 @@ type Props = {
 };
 
 const ShareModal = ({ shareTrackUrl, closeShareModal }: Props) => {
+  const copyAlert = useRef<HTMLParagraphElement>();
+
+  const copyAndClose = (): void => {
+    navigator.clipboard.writeText(shareTrackUrl);
+    copyAlert.current.classList.add(styles.copied__visible);
+
+    setTimeout(() => {
+      closeShareModal();
+    }, 1300);
+  };
+
   return (
     <div role="dialog" className={styles.container}>
       <div className={styles.button__close} onClick={closeShareModal}></div>
-
+      <p className={styles.copied} ref={copyAlert}>
+        Copied 👍
+      </p>
       <article className={styles.box}>
-        <h2 className={styles.header}>Share this url</h2>
+        <h2 className={styles.header}>Copy the link to share!</h2>
         <section className={styles.link__container}>
           <p className={styles.link}>{shareTrackUrl}</p>
         </section>
         <button
           className={styles.button}
           onClick={() => {
-            navigator.clipboard.writeText(shareTrackUrl);
+            copyAndClose();
           }}
         >
           <AiOutlineCopy />
