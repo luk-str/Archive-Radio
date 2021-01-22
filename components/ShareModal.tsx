d0d2d8e09366e97e85f58694860903a069421b2b
luk-str <lstrociak@gmail.com>
2021-01-22 14:32:07 +0100

    Add fade out when closing share modal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -9,33 +9,42 @@ type Props = {
 
 const ShareModal = ({ shareTrackUrl, closeShareModal }: Props) => {
   const copyAlert = useRef<HTMLParagraphElement>();
+  const container = useRef<HTMLDivElement>();
 
   const copyAndClose = (): void => {
     navigator.clipboard.writeText(shareTrackUrl);
     copyAlert.current.classList.add(styles.copied__visible);
+    container.current.classList.add(styles.container__closeWithDelay);
 
     setTimeout(() => {
       closeShareModal();
-    }, 1300);
+    }, 1400);
+  };
+
+  const close = (): void => {
+    container.current.classList.add(styles.container__close);
+
+    setTimeout(() => {
+      closeShareModal();
+    }, 400);
   };
 
   return (
-    <div role="dialog" className={styles.container}>
-      <div className={styles.button__close} onClick={closeShareModal}></div>
+    <div role="dialog" className={styles.container} ref={container}>
+      <div className={styles.button__close} onClick={() => close()}></div>
+
       <p className={styles.copied} ref={copyAlert}>
         Copied 👍
       </p>
+
       <article className={styles.box}>
         <h2 className={styles.header}>Copy the link to share!</h2>
+
         <section className={styles.link__container}>
           <p className={styles.link}>{shareTrackUrl}</p>
         </section>
-        <button
-          className={styles.button}
-          onClick={() => {
-            copyAndClose();
-          }}
-        >
+
+        <button className={styles.button} onClick={() => copyAndClose()}>
           <AiOutlineCopy />
         </button>
       </article>
